72d9bbdd877d90f3ccd5ca81121fff43c51eec63
somerwest01 <albertorios0125@gmail.com>
2026-03-12 12:16:58 -0700

    Update analyze.js

--- pages/api/analyze.js
+++ pages/api/analyze.js
@@ -1,1 +1,21 @@
+import { parseDxf } from "../../lib/parseDxf"
 
+export default function handler(req, res) {
+
+  try {
+
+    const { file } = req.body
+
+    const result = parseDxf(file)
+
+    res.status(200).json(result)
+
+  } catch (err) {
+
+    res.status(500).json({
+      error: err.message
+    })
+
+  }
+
+}
